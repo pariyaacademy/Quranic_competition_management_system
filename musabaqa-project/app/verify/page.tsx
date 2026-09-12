9108162c46fe8154184a3d1b27a5b8f228678e37
pariyaacademy <pariyaacademyschool@gmail.com>
2026-09-12 07:29:36 +0100

    Delete musabaqa-project directory

--- musabaqa-project/app/verify/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getLocale } from "@/lib/i18n/locale";
-import { getDictionary } from "@/lib/i18n/dictionaries";
-import VerifyForm from "./VerifyForm";
-
-export default function VerifyPage() {
-  const t = getDictionary(getLocale());
-  return <VerifyForm t={t.verify} />;
-}
